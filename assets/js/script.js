72b416b31f58f8cda9627997dc62392ed3a97883
captaiiinsolo <solo.d.santos@gmail.com>
2023-01-12 00:30:10 -0800

    fetch accu weather location API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,16 @@
-// API test
-var location = "San Francisco";
-var accuWeatherAPIKey = "G7TFrvoMDfSH4fn8av5CZDJviR257GdG";
-var accuWeatherURL = "http://dataservice.accuweather.com/locations/v1/cities/search" + accuWeatherAPIKey + "&q=" + location;
+function getAPI() {
+    var searchCity = "san Francisco";
+    var APIKey = "G7TFrvoMDfSH4fn8av5CZDJviR257GdG";
+    var requestURL = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=" + APIKey + "&q=" + searchCity;
 
-$.ajax({
-    url: accuWeatherURL,
-    method: "GET"
-}).then(function(response) {
-    console.log(response);
-})
+   fetch(requestURL)
+   .then(function(response) {
+    return response.json();
+   })
+   .then(function(data) {
+    console.log(data);
+   });
+}
+
+getAPI();
+
