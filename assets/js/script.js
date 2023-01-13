14ec85d8cb552d824a39bcfba5a55a15af96a413
captaiiinsolo <solo.d.santos@gmail.com>
2023-01-12 18:21:48 -0800

    add adzuna job board api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
-// Location API from Accuweather
-function getAPI() {
-      var searchCity = "san Francisco";
+// Calls the AccuWeather API (Nested Promise - First call to Location API to get location Key. Second Call to daily forecast API for weather information)
+function getAccuWeatherAPI() {
+      var searchCity = "san diego";
       var APIKey = "G7TFrvoMDfSH4fn8av5CZDJviR257GdG";
       var requestURL = "http://dataservice.accuweather.com/locations/v1/cities/search?apikey=" + APIKey + "&q=" + searchCity;
   
@@ -25,4 +25,26 @@ function getAPI() {
      
   }
 
-  getAPI();
+//   getAccuWeatherAPI();
+
+
+
+
+// Calls the adzuna job board API
+  function getJobsAPI() {
+    var appID = "a1161bda";
+    var jobsAPIKey = "3cbd548d24f2c7935ae4266b18c9a165";
+    var jobsURL = "https://api.adzuna.com/v1/api/jobs/us/search/1?app_id=" + appID + "&app_key=" + jobsAPIKey + "&results_per_page=25&what=software%20engineer";
+
+    fetch(jobsURL)
+        .then(function(response) {
+        return response.json();
+        })
+        .then(function(data) {
+        console.log(data);
+        });
+  }
+
+//   getJobsAPI();
+
+
